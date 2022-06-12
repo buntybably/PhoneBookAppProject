d7b15fddb27e10b05d370c1dff23197dca0025a8
buntybably <nikhilratnaparkhi12@gmail.com>
2022-06-12 14:18:43 +0530

    PHONE 02 : Create SpringBootStarter Project

---
+++ src/test/java/com/bikkadIT/PhoneBookApplication/PhoneBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bikkadIT.PhoneBookApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
